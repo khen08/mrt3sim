2bbf52524d9a1b6b5e9a8a3602f652412db840fe
Luke Andrei Ruego <LRuego20@gmail.com>
2025-04-23 02:06:24 +0800

    Remove unused fields from STATIONS and TRAINS models in schema.prisma, delete migration files, and enhance database setup with new station and train initialization logic.

--- backend/src/database/setup.py
+++ backend/src/database/setup.py
@@ -10,8 +10,10 @@ class InitializeDB_Data:
         base_date = self.get_datetime_from_csv()
         self.start_time = datetime.combine(base_date, time(hour=5, minute=0))
         self.end_time = datetime.combine(base_date, time(hour=22, minute=0))
-        print(self.start_time.isoformat(), self.end_time.isoformat())
+        
+        #print("\n", pd.DataFrame(config),"\n")
         self.save_simulation_data(scheme_type='Regular')
+        self.save_simulation_data(scheme_type='Skip-stop')
 
     def save_simulation_data(self, scheme_type):
         # Create a new simulation record and capture the returned object
@@ -26,21 +28,100 @@ class InitializeDB_Data:
                 'PASSENGER_DATA_FILE': self.file_path
             }
         )
+
+        simulation_entry_id = simulation_entry.SIMULATION_ID 
+
+        self.initialize_stations(scheme_type=scheme_type, simulation_id=simulation_entry_id)
+
+    def initialize_stations(self, scheme_type, simulation_id):
+        station_names = self.config['station_names']
+
+        if scheme_type == 'Regular':
+            for station_id, station in enumerate(station_names, start=1):
+                db.stations.create(
+                    data={
+                        'SIMULATION_ID': simulation_id,
+                        'STATION_ID': station_id,
+                        'STATION_NAME': station,
+                        'STATION_TYPE': 'AB',
+                        'IS_TERMINUS': False if station_id != 1 else True
+                    }
+                )
+        else:
+            for station_id, (station, station_type) in enumerate(zip(station_names, DEFAULT_SCHEME), start=1):
+                db.stations.create(
+                    data={
+                        'SIMULATION_ID': simulation_id,
+                        'STATION_ID': station_id,
+                        'STATION_NAME': station,
+                        'STATION_TYPE': station_type,   
+                        'IS_TERMINUS': False if station_id != 1 else True
+                    }
+                )
         
-        simulation_entry_id = simulation_entry.simulation_id 
-        self.initialize_stations(simulation_entry_id)
+        self.initialize_track_segments(scheme_type=scheme_type, simulation_id=simulation_id)
 
-    def initialize_stations(self, simulation_entry_id):
-        print(simulation_entry_id)
+    def initialize_track_segments(self, scheme_type, simulation_id):
+        station_distances = self.config['station_distances']
+        station_count = len(self.config['station_names'])
+        # Southbound Segments
+        for idx, distance in enumerate(station_distances, start=1):
+            db.track_segments.create(
+                data={
+                    'SIMULATION_ID': simulation_id,
+                    'START_STATION_ID': idx,
+                    'END_STATION_ID': idx + 1,
+                    'DISTANCE': distance * 1000,
+                    'DIRECTION': 'southbound'
+                }
+                )
+        
+        # Northbound Segments
+        for idx, distance in enumerate(reversed(station_distances)):
+            db.track_segments.create(
+                data={
+                    'SIMULATION_ID': simulation_id,
+                    'START_STATION_ID': station_count - idx,
+                    'END_STATION_ID': station_count - idx - 1,
+                    'DISTANCE': distance * 1000,
+                    'DIRECTION': 'northbound'
+                    }
+                )
+        
+        self.initialize_train_specs(scheme_type=scheme_type, simulation_id=simulation_id)
+    def initialize_train_specs(self, scheme_type, simulation_id):
+        train_specs_entry = db.train_specs.create(
+            data={
+                'SIMULATION_ID': simulation_id,
+                'SPEC_NAME': 'REGULAR TRAIN',
+                'MAX_CAPACITY': self.config['maxCapacity'],
+                'CRUISING_SPEED': self.config['maxSpeed'],
+                'PASSTHROUGH_SPEED': 20,# fix this
+                'ACCEL_RATE': self.config['acceleration'],
+                'DECEL_RATE': self.config['deceleration'],
+            }
+        )
 
-    def initialize_track_segments(self):
-        pass
-    
-    def initialize_trains(self):
-        pass
+        train_specs_entry_id = train_specs_entry.SPEC_ID
 
-    def initialize_train_movements(self):
-        pass
+        self.initialize_trains(scheme_type=scheme_type, spec_id=train_specs_entry_id, simulation_id=simulation_id)
+
+    def initialize_trains(self, scheme_type, spec_id, simulation_id):
+        train_count = 0
+        # Get the maximum train count from the service periods
+        for period in DEFAULT_SERVICE_PERIODS:
+            train_count = max(train_count, period['train_count'])
+        
+        for train_id in range(1, train_count + 1):
+            train_type = "AB" if scheme_type == "Regular" else "B" if train_id % 2 == 0 else "A"
+            db.trains.create(
+                data={
+                    'SIMULATION_ID': simulation_id,
+                    'TRAIN_ID': train_id,
+                    'SERVICE_TYPE': train_type,
+                    'SPEC_ID': spec_id,
+                }
+            )
 
     def initialize_passengers(self):
         pass
